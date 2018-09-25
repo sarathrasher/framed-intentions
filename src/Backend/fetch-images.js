88579a30351aaf90797e70fac1924b6da37320f0
Hannah <hrglasser94@gmail.com>
2018-09-25 13:26:58 -0400

    updated the object that is being returned so that it includes the small url and the description of the image. Also changed the query so that it returns squarish photos.

--- src/Backend/fetch-images.js
+++ src/Backend/fetch-images.js
@@ -4,14 +4,14 @@ const fetch = require('node-fetch');
 let fetchImages = (req, res) => {
  let query = (req.url.split('/').slice(-1))[0];
   console.log(query);
-  let fetchPromise = fetch(`https://api.unsplash.com/search/photos/?page=1&per_page=9&query=${query}&client_id=${UNSPLASH_API_KEY}`)
+  let fetchPromise = fetch(`https://api.unsplash.com/search/photos/?page=1&per_page=9&query=${query}&orientation=squarish&client_id=${UNSPLASH_API_KEY}`)
   .then(response => {
   return response.json();
   })
   .then(products => products.results)
   .then(results => results.map(result => {
     return (
-      { id: result.id, rawURL: result.urls.raw }
+      { id: result.id, description: result.description, smallURL: result.urls.small }
     )
   })).then(resultsObjects => res.end(resultsObjects))
   .catch(err => console.log(err));
