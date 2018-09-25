a9baa59cd1fdade0df9b716a13e07a4f9dd5dd6d
Hannah <hrglasser94@gmail.com>
2018-09-24 22:06:32 -0400

    Created a basic search input. Still needs to be styled and made more interactive.

---
+++ src/Frontend/ImageSearchBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+let ImageSearchBar = () => {
+  return (
+    <div className="input-group md-form form-sm form-2 pl-0">
+      <input className="form-control my-0 py-1 cyan-border" type="text" placeholder="Search" aria-label="Search"></input>
+      <div className="input-group-append">
+        <span className="input-group-text cyan lighten-2" id="basic-text1"><i className="fa fa-search text-grey" aria-hidden="true"></i></span>
+      </div>
+    </div>
+    )
+}
+
+export default ImageSearchBar;
