aff82b5023059abd082a2342ecc3b99aef3239e1
Hannah <hrglasser94@gmail.com>
2018-09-24 21:34:10 -0400

    Created a fetch request that will take in a query and pull image results from unsplash based upon that query.

---
+++ src/Backend/fetch-images.js
@@ -0,0 +1,16 @@
+const { UNSPLASH_API_KEY } = require('../../secrets');
+const fetch = require('node-fetch');
+
+let query = 'dogs';
+
+fetch(`https://api.unsplash.com/search/photos/?page=1&per_page=9&query=${query}&client_id=${UNSPLASH_API_KEY}`)
+  .then(response => {
+  return response.json();
+  })
+  .then(products => products.results)
+  .then(results => results.map(result => {
+    return (
+      { id: result.id, raw: result.urls.raw, full: result.urls.full, regular: result.urls.regular, small: result.urls.small, thumb: result.urls.thumb }
+    )
+  })).then(resultsObjects => console.log(resultsObjects))
+
