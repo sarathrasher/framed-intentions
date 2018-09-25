49411097decc9e41a3683377f77f253803391989
Sara Muntean <munteansr@gmail.com>
2018-09-25 15:32:32 -0400

    Revised board to eliminate squares and adjusted CSS to match new size.

--- src/Frontend/Components/BoardTemplate.js
+++ src/Frontend/Components/BoardTemplate.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import BoardSquare from './BoardSquare';
+// import BoardSquare from './BoardSquare';
 
-let squares = [];
-for (let i = 1; i < 10; i++) {
-  squares.push(<BoardSquare key={i} number={i}/>)
-}
+// let squares = [];
+// for (let i = 1; i < 10; i++) {
+//   squares.push(<BoardSquare key={i} number={i}/>)
+// }
 
 let BoardTemplate = () => 
   <div className='board-container'>
     <div className='board'>
-      {squares}
+      {/* {squares} */}
     </div>
   </div>
 
