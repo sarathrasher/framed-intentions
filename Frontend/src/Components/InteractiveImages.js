9e7838e0d899578f95174d14362352c0b8bfa7cf
Sara Muntean <munteansr@gmail.com>
2018-09-27 15:26:04 -0400

    WIP. Added props to image, but need to figure how to render at those points.

--- Frontend/src/Components/InteractiveImages.js
+++ Frontend/src/Components/InteractiveImages.js
@@ -20,6 +20,10 @@ const InteractiveImages = (props) =>
     className='sidebar-image'
     src={props.image.smallURL}
     alt={props.image.description}
+    data-x={props.image.location[0]}
+    data-y={props.image.location[1]}
+    width={props.image.size[0]}
+    height={props.image.size[1]}
     ></img>
   </InteractWrapper>
 
