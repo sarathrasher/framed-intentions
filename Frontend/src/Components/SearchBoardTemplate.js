42db1589404a8b669d8a9b8ced714522febe06f8
Hannah <hrglasser94@gmail.com>
2018-09-26 22:56:03 -0400

    Changed save button into a link that links to our create page.

--- Frontend/src/Components/SearchBoardTemplate.js
+++ Frontend/src/Components/SearchBoardTemplate.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import SearchBoardSquare from './SearchBoardSquare';
+import { Link } from 'react-router-dom';
 
-// let saveImages = (event) => {
-//   event.preventDefault();
-//   //this function will cause navigation to the create board to happen
-// }
+let saveImages = (event) => {
+  event.preventDefault();
+  //this function will cause navigation to the create board to happen
+}
 
 const SearchBoardTemplate = (props) => {
   return (
-    <form className='search-images-container'>
+    <form className='search-images-container' onSubmit={saveImages}>
       <div className='search-images-board'>
         {props.images.map(image => <SearchBoardSquare key={image.id} image={image}/>)}
       </div>
-      <button type="submit" className="btn btn-danger">Save</button>
+      <Link to="/create">
+        <button type="submit" className="btn btn-danger">Save</button>
+      </Link>
   </form>)
 }
 
